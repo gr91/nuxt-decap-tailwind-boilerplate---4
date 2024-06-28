fdf4c1df3730c7e8f0ec41201146ff7f0dee4149
gr91 <g.romagno91@gmail.com>
2024-06-28 18:02:00 +0200

    change nav

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,6 +1,9 @@
 <template>
     <main class="mx-auto max-w-4xl space-y-6">
-    <NuxtLink to="/blog">&lt; Back</NuxtLink>
+    <!-- <NuxtLink to="/blog">&lt; Back</NuxtLink> -->
+    <nav class="mb-8" aria-label="go back">
+        <router-back class="block" />
+      </nav>
     <h1 class="text-4xl">
       {{ post.title }}
     </h1>
